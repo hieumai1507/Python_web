e88b23806abaa1fa376f522a19cdafce1b92f7b7
hieumai1507 <114709586+hieumai1507@users.noreply.github.com>
2024-09-16 16:16:04 +0700

    fix db

--- ecommerce_app/app/models.py
+++ ecommerce_app/app/models.py
@@ -8,20 +8,19 @@ CATEGORY_CHOICES = (
   ('MS', 'Milkshake'),
   ('PN', 'Paneer'),
   ('GH', 'Ghee'),
-  ('CZ', 'chess'),
+  ('CZ', 'Cheese'),
   ('IC', 'Ice-Creams'),
 )
-
 
 class Product(models.Model):
   title = models.CharField(max_length=100)
   selling_price = models.FloatField()
   discounted_price = models.FloatField()
   description = models.TextField()
-  composition = models.TextField()
-  prodapp = models.TextField()
+  composition = models.TextField(default="")
+  prodapp = models.TextField(default="")
   category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
   product_image = models.ImageField(upload_to='product')
-  def _str_(self):
+
+  def __str__(self):
     return self.title
-#tao nguoi viet Nam
--- ecommerce_app/app/views.py
+++ ecommerce_app/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
-def home(req):
-  return render(req, "app/home.html");
+def home(request):
+  return render(request, "app/home.html");

e5ec4162603a9aea6c65571cb2dc44e76f583363
TrinhHuy <ps.quanghuy@gmail.com>
2024-09-17 18:44:52 +0700

    updateAddress

--- ecommerce_app/app/views.py
+++ ecommerce_app/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from . models import Product
 from django.db.models import Count
@@ -71,8 +71,23 @@ def address(request):
 
 class updateAddress(View):
   def get(self, request,pk):
-    form = CustomerProfileForm()
+    add = Customer.objects.get(pk=pk)
+    form = CustomerProfileForm(instance= add)
     return render(request, 'app/updateAddress.html',locals())
   def post(self, request,pk):
     form = CustomerProfileForm(request.POST)
-    return render(request, 'app/updateAddress.html',locals())
+    
+    if form.is_valid():
+      add = Customer.objects.get(pk=pk)
+      add.name = form.cleaned_data['name']
+      add.locality = form.cleaned_data['locality']
+      add.city = form.cleaned_data['city']
+      add.mobile = form.cleaned_data['mobile']
+      add.state = form.cleaned_data['state']
+      add.zipcode = form.cleaned_data['zipcode']
+      add.save()
+      messages.success(request, "Ho so ban da cap nhat")
+    else:
+        messages.warning(request, "Nhap data vao")
+    return redirect('address')
+

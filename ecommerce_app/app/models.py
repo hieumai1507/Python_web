637ed0d9f88379048c7c752f58c40d5b77edbb73
TrinhHuy <ps.quanghuy@gmail.com>
2024-09-17 17:00:43 +0700

    address, login, profile

--- ecommerce_app/app/models.py
+++ ecommerce_app/app/models.py
@@ -1,6 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATE_CHOICES =(
+    ('An Giang', 'An Giang'),
+    ('Bà Rịa - Vũng Tàu', 'Bà Rịa - Vũng Tàu'),
+    ('Bắc Giang', 'Bắc Giang'),
+    ('Bắc Kạn', 'Bắc Kạn'),
+    ('Bạc Liêu', 'Bạc Liêu'),
+    ('Bắc Ninh', 'Bắc Ninh'),
+    ('Bến Tre', 'Bến Tre'),
+    ('Bình Định', 'Bình Định'),
+    ('Bình Dương', 'Bình Dương'),
+    ('Bình Phước', 'Bình Phước'),
+    ('Bình Thuận', 'Bình Thuận'),
+    ('Cà Mau', 'Cà Mau'),
+    ('Cần Thơ', 'Cần Thơ'),  # City
+    ('Cao Bằng', 'Cao Bằng'),
+    ('Đà Nẵng', 'Đà Nẵng'),  # City
+    ('Đắk Lắk', 'Đắk Lắk'),
+    ('Đắk Nông', 'Đắk Nông'),
+    ('Điện Biên', 'Điện Biên'),
+    ('Đồng Nai', 'Đồng Nai'),
+    ('Đồng Tháp', 'Đồng Tháp'),
+    ('Gia Lai', 'Gia Lai'),
+    ('Hà Giang', 'Hà Giang'),
+    ('Hà Nam', 'Hà Nam'),
+    ('Hà Nội', 'Hà Nội'),  # City
+    ('Hà Tĩnh', 'Hà Tĩnh'),
+    ('Hải Dương', 'Hải Dương'),
+    ('Hải Phòng', 'Hải Phòng'),  # City
+    ('Hậu Giang', 'Hậu Giang'),
+    ('Hồ Chí Minh', 'Hồ Chí Minh'),  # City
+    ('Hòa Bình', 'Hòa Bình'),
+    ('Hưng Yên', 'Hưng Yên'),
+    ('Khánh Hòa', 'Khánh Hòa'),
+    ('Kiên Giang', 'Kiên Giang'),
+    ('Kon Tum', 'Kon Tum'),
+    ('Lai Châu', 'Lai Châu'),
+    ('Lâm Đồng', 'Lâm Đồng'),
+    ('Lạng Sơn', 'Lạng Sơn'),
+    ('Lào Cai', 'Lào Cai'),
+    ('Long An', 'Long An'),
+    ('Nam Định', 'Nam Định'),
+    ('Nghệ An', 'Nghệ An'),
+    ('Ninh Bình', 'Ninh Bình'),
+    ('Ninh Thuận', 'Ninh Thuận'),
+    ('Phú Thọ', 'Phú Thọ'),
+    ('Phú Yên', 'Phú Yên'),
+    ('Quảng Bình', 'Quảng Bình'),
+    ('Quảng Nam', 'Quảng Nam'),
+    ('Quảng Ngãi', 'Quảng Ngãi'),
+    ('Quảng Ninh', 'Quảng Ninh'),
+    ('Quảng Trị', 'Quảng Trị'),
+    ('Sóc Trăng', 'Sóc Trăng'),
+    ('Sơn La', 'Sơn La'),
+    ('Tây Ninh', 'Tây Ninh'),
+    ('Thái Bình', 'Thái Bình'),
+    ('Thái Nguyên', 'Thái Nguyên'),
+    ('Thanh Hóa', 'Thanh Hóa'),
+    ('Thừa Thiên Huế', 'Thừa Thiên Huế'),
+    ('Tiền Giang', 'Tiền Giang'),
+    ('Trà Vinh', 'Trà Vinh'),
+    ('Tuyên Quang', 'Tuyên Quang'),
+    ('Vĩnh Long', 'Vĩnh Long'),
+    ('Vĩnh Phúc', 'Vĩnh Phúc'),
+    ('Yên Bái', 'Yên Bái'),
+)
 CATEGORY_CHOICES = (
   ('CR', 'Curd'),
   ('ML', 'Milk'),
@@ -24,3 +90,15 @@ class Product(models.Model):
 
   def __str__(self):
     return self.title
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+    
+    def __str__(self):
+        return self.name
--- ecommerce_app/app/views.py
+++ ecommerce_app/app/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views import View
 from . models import Product
 from django.db.models import Count
-from . forms import CustomerRegistationForm
+from . forms import CustomerRegistationForm, CustomerProfileForm
 from django.contrib import messages
+from .models import Customer
 # Create your views here.
 def home(request):
   return render(request, "app/home.html");
@@ -41,3 +42,37 @@ class CustomerRegistationView(View):
     else:
       messages.warning(request, "Invalid Input Data")
     return render(request,"app/customerregistration.html", locals())
+
+class ProfileView(View):
+  def get(self, request):
+    form = CustomerProfileForm()
+    return render(request, 'app/profile.html',locals())
+  def post(self, request):
+    form = CustomerProfileForm(request.POST)
+    if form.is_valid():
+
+      user = request.user
+      name = form.cleaned_data['name']
+      locality = form.cleaned_data['locality']
+      city = form.cleaned_data['city']
+      mobile = form.cleaned_data['mobile']
+      state = form.cleaned_data['state']
+      zipcode = form.cleaned_data['zipcode']
+      reg = Customer(user=user, name = name, locality = locality, city = city, mobile = mobile, state = state, zipcode= zipcode)
+      reg.save()
+      messages.success(request, "Profile ban da luu")
+    else:
+      messages.warning(request, "Moi ban nhap du lieu")
+    return render(request, 'app/profile.html',locals())
+
+def address(request):
+  add = Customer.objects.filter(user = request.user)   
+  return render(request, 'app/address.html',locals()) 
+
+class updateAddress(View):
+  def get(self, request,pk):
+    form = CustomerProfileForm()
+    return render(request, 'app/updateAddress.html',locals())
+  def post(self, request,pk):
+    form = CustomerProfileForm(request.POST)
+    return render(request, 'app/updateAddress.html',locals())
